242d077610d5045ccfee6dbb00f54bbdf6866504
mudasar187 <ahmmud16@hotmail.com>
2018-03-15 19:29:56 +0100

    Add /GET todos route

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 // Post route
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
+    let todo = new Todo({
       text: req.body.text
     });
   
@@ -25,7 +25,14 @@ app.post('/todos', (req, res) => {
     });
   });
 
-
+// Get route
+app.get('/todos', (req,res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
 
 // Create the port this server is listen to
 app.listen(3000, () => {
